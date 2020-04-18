b490b38c44825755d91bac8e582e91f822478462
Matt <mjmccoy@stanford.edu>
2020-04-18 13:24:55 -0700

    PickUp and DestroyOnCollide
    PickUp and DestroyOnCollide

---
+++ Assets/Scripts/DestroyOnCollide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollide : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Monster")
+        {
+            Debug.Log("Item contacted");
+            Destroy(gameObject);
+        }
+    }
+}
