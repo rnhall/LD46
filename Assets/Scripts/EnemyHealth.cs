21f055d0625945c7d2a2b4f951ccfb854aaca4b5
Matt <mjmccoy@stanford.edu>
2020-04-18 20:24:49 -0700

    test
    test

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float Health = 25f;
+
+    public void TakeDamage(float annt)
+    {
+        Health -= annt;
+        if (Health <= 0)
+        {
+            print("Enemy has deid!!!");
+        }
+
+        print("Enemy took some damage");
+    }
+}
